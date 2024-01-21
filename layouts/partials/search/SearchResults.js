c8907b761c4046e7ae593c7681301470d0d7db43
bari95 <elibarikaneno@gmail.com>
2024-01-21 19:03:56 +0300

    editing search functionality

--- layouts/partials/search/SearchResults.js
+++ layouts/partials/search/SearchResults.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import TranslationResult from './TranslationResult';
 import ImageResult from './ImageResult';
@@ -5,7 +7,7 @@ import ExampleSentences from './ExampleSentences';
 
 const SearchResults = ({results}) => {
   return (
-    <div className="search-results grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+    <div className="search-results bg-white mt-[-20px] grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       <TranslationResult translation={results.translation} />
       <ImageResult imageUrl={results.imageUrl} />
       <ExampleSentences examples={results.examples} />
