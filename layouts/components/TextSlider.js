8813dab3cb764f91fff3a401af6eb7c17f288c95
bari95 <elibarikaneno@gmail.com>
2024-02-12 19:12:38 +0300

    update text slider

--- layouts/components/TextSlider.js
+++ layouts/components/TextSlider.js
@@ -6,11 +6,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const CustomNextArrow = (props) => (
-  <FaArrowRight {...props} className="slick-arrow custom-next-arrow absolute top-1/2 right-0 transform -translate-y-1/2 text-3xl text-white transition duration-300 ease-in-out hover:text-yellow-350" />
+  <FaArrowRight {...props} className="slick-arrow custom-next-arrow absolute top-1/2 right-0 transform -translate-y-1/2 text-3xl text-white transition duration-300 ease-in-out hover:text-yellow-400" />
 );
 
 const CustomPrevArrow = (props) => (
-  <FaArrowLeft {...props} className="slick-arrow custom-prev-arrow absolute top-1/2 left-0 transform -translate-y-1/2 text-3xl text-white transition duration-300 ease-in-out hover:text-yellow-350" />
+  <FaArrowLeft {...props} className="slick-arrow custom-prev-arrow absolute top-1/2 left-0 transform -translate-y-1/2 text-3xl text-white transition duration-300 ease-in-out hover:text-yellow-400" />
 );
 
 const TextSlider = ({ wordOfTheDayArray }) => {
@@ -28,9 +28,11 @@ const TextSlider = ({ wordOfTheDayArray }) => {
     <div className="w-4/5 mx-auto relative">
       <Slider {...settings} className="rounded overflow-hidden">
         {wordOfTheDayArray.map((wordObject, index) => (
-          <div key={index} className="text-center p-8 bg-gradient-to-r from-teal-400 to-blue-500 text-white rounded-md">
+          <div key={index} className="text-center p-8 bg-gradient-to-r from-teal-400 to-blue-500 text-white">
             <h1>{wordObject.word}</h1>
-            <p>{wordObject.meaning}</p>
+            <div className="meaning-container border-white border-4 rounded-md p-4 mt-4 mx-auto max-w-sm">
+              <p className="text-lg font-bold text-white">{wordObject.meaning}</p>
+            </div>
           </div>
         ))}
       </Slider>
