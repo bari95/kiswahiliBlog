6cbcc3ddb02d1a4deb99b545fdbc419f04ea0605
elidistro7 <elidistro@gmail.com>
2025-06-19 03:58:52 +0300

    hello

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import TwSizeIndicator from "@components/TwSizeIndicator";
+
 import config from "@config/config.json";
 import { Head, Html, Main, NextScript } from "next/document";
 import {Analytics} from "@vercel/analytics/react";
@@ -76,7 +76,7 @@ const Document = () => {
       <body>
         <Main />
         <Analytics />
-        <TwSizeIndicator />
+       
         
 
         {/* Google Translate container */}
